44431bf9e0442242675d0c57c9946e6a8566a2d0
warroyo <warroyo7199008@gmail.com>
2018-11-02 11:51:32 -0600

    update version to v3

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ namespace CfSampleAppDotNetCore
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Congratulations! this is a sample .net app. V2");
+                return context.Response.WriteAsync("Congratulations! this is a sample .net app. V3");
             });
         }
     }
